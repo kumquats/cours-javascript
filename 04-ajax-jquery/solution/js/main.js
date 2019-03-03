911448a9a7f1449393b0f01e8447f6ba366560e1
thomas <tf@kumquats.fr>
2019-03-03 21:53:48 +0100

    04 ajax jquery : solution jquery

--- 04-ajax-jquery/solution/js/main.js
+++ 04-ajax-jquery/solution/js/main.js
@@ -2,6 +2,7 @@
 import HomePage from './HomePage.js';
 import PageRenderer from './PageRenderer.js';
 import AddPizzaPage from './AddPizzaPage.js';
+import $ from 'jquery';
 
 PageRenderer.titleElement = document.querySelector('.pageTitle');
 PageRenderer.contentElement = document.querySelector('.pizzasContainer');
@@ -64,11 +65,35 @@ fetch('./news.html')
 	.then( (response:Response) => response.text() )
 	.then( displayNews );
 
-function renderHome(data:Array<{nom:string, base:string, prix_petite:number, prix_grande:number}>):void{
-	homePage.data = data;
+// A.4. Appeler une API REST/JSON en GET
+function renderHome(data:?Array<{nom:string, base:string, prix_petite:number, prix_grande:number}> = null):void{
+	if ( data ) {
+		homePage.data = data;
+	}
 	PageRenderer.renderPage(homePage);
 }
-
 fetch('http://localhost:8080/api/v1/pizzas')
 	.then( (response:Response) => response.json() )
-	.then( renderHome );
+	.then( renderHome )
+	// B.3.6. retirer la classe is-loading
+	.then( () => $('.pizzasContainer').removeClass('is-loading') )
+	// B.4.2. détecter le clic sur les liens de la home
+	.then( pizzasContainer => pizzasContainer.find('a').click(event => {
+		event.preventDefault();
+		window.open(
+			$(event.currentTarget).attr('href'),
+			'popup',
+			'width=350,height=200,menubar=0,toolbar=0,location=0,personalbar=0,status=0'
+		);
+	}));
+
+// B.2. La fonction $()
+// console.log( $('a.navbar-brand').html() );
+// $('a.navbar-brand').html('jQuery forever');
+// $('a').html('jQuery forever');
+$('.pizzasContainer').addClass('is-loading');
+
+// B.4.1. auc clic sur le logo on affiche la homepage
+$('a.navbar-brand').on( 'click', event => renderHome() );
+
+
